97e12dea252ebe374c48610affc96b14480e50ea
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-05-15 23:25:30 -0700

    Ch 2 project program 7 added

--- Problem_Solving_w_CPP_Savitch_10e_Solutions/Chapter_2.h
+++ Problem_Solving_w_CPP_Savitch_10e_Solutions/Chapter_2.h
@@ -358,5 +358,25 @@ void Ch2_Programming_Project_6() {
 }
 
 void Ch2_Programming_Project_7(){
-    
+	double item_purchase_date = 0.0;
+	double item_cost = 0.0;
+	double item_inflation_rate = 0.0;
+	
+	item_cost = inputDouble("Cost of items ($):", true);
+	item_purchase_date = inputDouble("When will these items be purchased (years)? ", true);
+	item_inflation_rate = inputDouble("Inflation rate of the item (%): ", true);
+	
+	item_inflation_rate /= 100;
+	
+	for(int i = 0; i < item_purchase_date; i++)
+		item_cost += item_cost * item_inflation_rate;
+	
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+	
+	cout << endl;
+	cout << "Cost: $" << item_cost << endl;
+	cout << "Purchase date: " << item_purchase_date << " year(s) from now" << endl;
+	cout << "Inflation rate: " << item_inflation_rate << "%" << endl;
 }
