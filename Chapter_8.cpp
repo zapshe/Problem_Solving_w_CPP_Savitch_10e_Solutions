664b640e57caf066146a7c5b4476f9a303ab7a1a
zapshe <56454733+zapshe@users.noreply.github.com>
2020-05-21 15:07:34 -0700

    Add files via upload

---
+++ Chapter_8.cpp
@@ -0,0 +1,37 @@
+// ConsoleApplication52.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <deque>
+
+void problemOne()
+{
+	/*
+	1. Write a program in which you declare a deque to store values of
+	type double, read in ten double numbers, and store them in the deque.
+	Then call the generic sort function to sort the numbers in
+	the deque and display the results.
+	*/
+
+	std::deque<double> in;
+
+	for (int i = 0; i < 10; i++)
+	{
+		double temp;
+		std::cout << "Enter A Double Value: ";
+		std::cin >> temp;
+		in.push_back(temp);
+		std::sort(in.begin(), in.end());
+	}
+
+	for (auto &i : in)
+	{
+		std::cout << i << ' ';
+	}
+}
+
+int main()
+{
+	problemOne();
+}
